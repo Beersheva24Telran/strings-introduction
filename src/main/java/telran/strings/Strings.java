8b3470c38c8263b86b5d961bf9949e5403bd2a62
Yuragranovsky <yuragranovsky@gmail.com>
2024-07-30 12:19:48 +0300

    HW #10 regex for syntax check of Java  names

--- src/main/java/telran/strings/Strings.java
+++ src/main/java/telran/strings/Strings.java
@@ -1,15 +1,18 @@
 package telran.strings;
 
+
 public class Strings {
     public static String firstName() {
-        //regex for strings starting with capital letter and rest as lowercase letters
-        //minimal length is 5 letters
+        // regex for strings startin with
+        // capital letter and rest as a
+        // lower case letters
+        // minimal length is 5 letters
         return "[A-Z][a-z]{4,}";
     }
+
     public static String javaVariable() {
-        //TODO
-        //regular expression for testing syntax of Java veriable names
-        //only ACII symbols are allowed
-        return "";
+        // regular expression for testing synthax
+        // of Java variable names
+        return "[a-zA-Z$]|[a-zA-Z$_][\\w\\d$]+";
     }
 }
--- src/test/java/telran/strings/RegularExpressionsTest.java
+++ src/test/java/telran/strings/RegularExpressionsTest.java
@@ -1,6 +1,31 @@
 package telran.strings;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class RegularExpressionsTest {
-    void javaVariableTest(){
-        //TODO
+    @Test
+    void javaVariableTrueTest(){
+        String regex = Strings.javaVariable();
+        
+        assertTrue("name".matches(regex));
+        assertTrue("a_1".matches(regex));
+        assertTrue("S".matches(regex));
+        assertTrue("$".matches(regex));
+        assertTrue("__".matches(regex)); //two underscore symbols
+        assertTrue("ab_cd_$12$".matches(regex));
+    }
+    @Test
+    void javaVariableFalseTest(){
+        String regex = Strings.javaVariable();
+        
+        assertFalse("1name".matches(regex));
+        assertFalse("1_a".matches(regex));
+        assertFalse("_".matches(regex));
+        assertFalse("#1".matches(regex));
+        assertFalse("_*".matches(regex)); //two underscore symbols
+        assertFalse("ab_cd_$12?".matches(regex));
+        assertFalse("ab_cd_$12 ".matches(regex));
     }
 }
